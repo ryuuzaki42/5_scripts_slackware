4139f29fec13f4822e4d4b67a5c538306387b0c5
João Batista <joao42lbatista@gmail.com>
2015-11-23 15:51:16 -0200

    update on codific.sh

--- codific.sh
+++ codific.sh
@@ -24,25 +24,24 @@
 #
 # Última atualização: 22/11/2015
 #
+
 if [ $# -ne 1 ] # verifica se foi passado o nome do arquivo
 then
-   echo "$(basename "$0"): Erro de Operandos"
-   echo "Use $0 nome.extensão do arquivo que deseja converter"
-   echo "Tente $0 --help para mais detalhes"
+   echo -e "\n$(basename "$0"): Erro de operandos"
+   echo "Use $0 nome.extensão (do arquivo que deseja converter)"
+   echo -e "Tente $0 --help para mais detalhes\n"
    exit 0
 fi
 
 ajuda () { #função de Ajuda
-  echo "#                                                             #"
-  echo "# Use o nome do arquivo (com a extensão) que deseja converter #"  
-  echo "# Ex.: $0 file.srt                                  #"
-  echo "#                                                             #" 
+  echo -e "\n# Use o nome do arquivo (com a extensão) que deseja converter #"
+  echo -e "# Ex.: $0 file.srt                                  #\n"
   exit 0
 }
 
 case "$1" in #Case virifica chamada da função ajuda
 '--help')
-  ajuda  
+  ajuda
 esac
 
 nomeArquivo="$1" #Nome do arquivo $1
@@ -50,9 +49,10 @@ if [ -e "$nomeArquivo" ]
 then
   echo >/dev/null
 else
-  echo -e "Arquivo passado por parâmetro não existe\nTente \"$0 --help\" ou com outro arquivo"
+  echo -e "\nArquivo passado por parâmetro não existe\nTente \"$0 --help\" ou com outro arquivo\n"
   exit 1
 fi
+
 tamanhoNome=$(echo "$nomeArquivo" | wc -m) #Calcula o tamanho do Nome e retirando retorno do wc
 tamanhoNome2=$((tamanhoNome - 5)) #Tamanho do nome do arquivo sem a extensão
 nomeArquivo2=$(echo "$nomeArquivo" | cut -c1-$tamanhoNome2) #Nome do arquivo sem extensão
@@ -66,12 +66,12 @@ then
   codFinal=utf-8
   iconv -f iso-8859-1 -t utf-8 "$nomeArquivo" > "$nomeArquivo2"_"$codFinal"."$extensao" #converter arquivo para utf-8 salvando em outro arquivo
 elif echo $codificacao | grep UTF-8 > /dev/null #verifica codificacao do arquivo é iso-8859
-then  
+then
   codInicial=utf-8
   codFinal=iso-8859 
   iconv -f utf-8 -t iso-8859-1 "$nomeArquivo" > "$nomeArquivo2"_"$codFinal"."$extensao" #converter arquivo para iso-8859 salvando em outro arquivo
-else #Em ultimo caso, nenhuma das duas codificações termina
-  echo -e "Codificação desconhecida\nArquivo não pode ser convertido"
+else #Em último caso, se o arquivo não for de nenhuma das duas codificações, o sccrit termina
+  echo -e "\nCodificação desconhecida\nArquivo não pode ser convertido\n"
   exit 1
 fi
 
@@ -80,7 +80,17 @@ then
   echo -e "Erro encontrado na execução do iconv\nTente $0 --help"
   exit 1
 else
-  echo -e "##Arquivo convertido com sucesso##\n\"$nomeArquivo\" de $codInicial para $codFinal"
+  echo -e "\n## Arquivo convertido com sucesso ##\n\"$nomeArquivo\" de $codInicial para $codFinal"
   echo "$nomeArquivo --> $nomeArquivo2"_"$codFinal.$extensao"
+  echo -e "\nSobrescrever o arquivo original?"
+  echo "(S)im, (n)ão"
+  read resposta
+  if [ $resposta = s ]
+  then
+    mv "$nomeArquivo2"_"$codFinal.$extensao" "$nomeArquivo"
+    echo -e "O arquivo foi sobrescrito\n Fim do script\n"
+  else
+    echo -e "O arquivo não foi sobrescrito\n Fim do script\n"
+  fi
 fi
 #
