fe1f3ed019cddf83d46985de0b6b73aae23937f6
João Batista <joao42lbatista@gmail.com>
2016-01-04 14:07:10 -0200

    Update 0install.r to be one script with you can install the another scripts in the main folder

--- 0install.r
+++ 0install.r
@@ -1,4 +1,10 @@
+#!/bin/sh
 
-Copie os arquivos para:
-# cp *.sh /usr/bin/
+## Copy the files to /usr/bin:
+    # Can just execut this script
+echo "This script will copy (cp *.sh) to /usr/bin/"
+scriptsFolder=`pwd`
+export scriptsFolder
+su - root -c "cd $scriptsFolder
+cp *.sh /usr/bin/"
 
