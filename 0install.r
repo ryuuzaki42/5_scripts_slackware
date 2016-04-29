b2d9378a4082dcae9e3a315e4d650e7fcb06b66f
João Batista <joao42lbatista@gmail.com>
2016-04-29 13:16:19 -0300

    Added "Be careful"

--- 0install.r
+++ 0install.r
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-## Copy the files to /usr/bin:
+## Copy the scripts int this folder to /usr/bin:
+    # Be careful, he overwrite the files, if they already exists
     # You can just execut this script
+#
+# Última atualização: 29/04/2016
+#
 echo "This script copy (cp *.sh) to /usr/bin/"
 scriptsFolder=`pwd`
 export scriptsFolder
 su - root -c "cd $scriptsFolder
 cp *.sh /usr/bin/"
 #
+
