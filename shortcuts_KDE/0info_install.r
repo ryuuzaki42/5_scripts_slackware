370112cd1873bc73219a93f86f0490ac24a18df2
João Batista <joao42lbatista@gmail.com>
2016-04-29 13:17:59 -0300

    Added shortcuts_KDE to kwrite, Lock Screen and Suspend using script from the above folder

---
+++ shortcuts_KDE/0info_install.r
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+## Copy the this shortcuts to your folder /home/$USER/.local/share/applications/
+    # Be careful, he overwrite the files, if they already exists
+    # You can just execut this script
+#
+# Última atualização: 29/04/2016
+#
+# Dica: Adicione no KDE-menu um atalho de teclado:
+# Lock Screen.desktop => ctrl + alt + l
+# Suspend.desktop     => ctrl + alt + s
+#
+echo "This script copy (cp *.sh) to /home/$USER/.local/share/applications/"
+cp *.desktop /home/$USER/.local/share/applications/
+#
+
