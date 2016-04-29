7cab0bcd5fa73ce24e59761341b62f8185d7a91d
João Batista <joao42lbatista@gmail.com>
2016-04-29 12:58:21 -0300

    Added script to run lightsOnPlusJB.sh at the pre-kde start

---
+++ lightsOnPlusJB_run_pre-KDE_start.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+#
+# Use this scrpit to load lightsOnPlusJB.sh in the KDE pre-start
+# Last change: 29/04/2016
+#
+# Don't forget to copy lightsOnPlusJB.sh to /usr/bin/
+# You can use the ./0install.r to do this
+#
+cd /usr/bin/
+/bin/bash lightsOnPlusJB.sh &
+#
