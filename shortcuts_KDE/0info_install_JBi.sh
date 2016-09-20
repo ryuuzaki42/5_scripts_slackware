65bfff673257543af387b0768ede1d4cb268cd3e
João Batista <joao42lbatista@gmail.com>
2016-09-20 14:50:17 -0300

    test before copy in the 0info_install_JBi.sh

--- shortcuts_KDE/0info_install_JBi.sh
+++ shortcuts_KDE/0info_install_JBi.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
 ## Copy the this shortcuts to your folder /home/$USER/.local/share/applications/
-    # Be careful, he overwrite the files, if they already exists
     # You can just execut this script
-#
-# Última atualização: 29/04/2016
-#
+
+# Last update: 20/09/2016
+
 # Dica: Adicione no KDE-menu um atalho de teclado:
 # Lock Screen.desktop => ctrl + alt + l
 # Suspend.desktop     => ctrl + alt + s
-#
-echo "This script copy (cp *.sh) to /home/$USER/.local/share/applications/"
-cp *.desktop /home/$USER/.local/share/applications/
+
+echo -e "This script copy (cp *.desktop) to /home/$USER/.local/share/applications/\n"
+
+echo "List of files that will be copied:"
+ls *.desktop
+echo -e "\t\nBe careful, will overwrite the files if they already exists\n"
+
+echo -en "Want continue and copy this files?\n(y)es - (n)o: "
+read continueCopy
+
+if [ "$continueCopy" == "y" ]; then
+    cp *.desktop /home/$USER/.local/share/applications/
+else
+    echo -e "\n\tThe Files was not copied"
+fi
+
+echo -e "\nEnd of the script\n"
