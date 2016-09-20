d8407db96bafc5b2904c2f4297bb9f830ccb8dfc
João Batista <joao42lbatista@gmail.com>
2016-09-20 12:50:17 -0300

    Some changes in the install script

--- 0install_scripts.sh
+++ 0install_scripts.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
 ## Copy the scripts int this folder to /usr/bin:
-    # Be careful, he overwrite the files, if they already exists
     # You can just execut this script
-#
-# Última atualização: 06/06/2016
-#
-echo "This script copy (cp *.sh) to /usr/bin/"
-scriptsFolder=`pwd`
-export scriptsFolder
-su - root -c "cd $scriptsFolder
-cp *.sh /usr/bin/"
+
+# Last update: 20/09/2016
+
+echo -e "This script copy (cp *.sh) to /usr/bin/\n"
+echo "List of files that will be copied:"
+
+listFiles=`ls *.sh`
+listFiles=`echo "$listFiles" | grep -v "${0:2}"`
+echo "$listFiles"
+
+echo -e "\t\nBe careful, will overwrite the files if they already exists\n"
+
+echo -en "Want continue and copy this files?\n(y)es - (n)o: "
+read continueCopy
+
+if [ "$continueCopy" == "y" ]; then
+    scriptsFolder=`pwd`
+    export scriptsFolder
+    su - root -c "cd $scriptsFolder
+    cp *.sh /usr/bin/"
+fi
+echo -e "\nEnd of the script\n"
 #
