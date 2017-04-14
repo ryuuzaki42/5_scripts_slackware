6ac2b42d6c662df78fe921c5e5d802b6a8bf6419
João Batista <joao42lbatista@gmail.com>
2017-04-14 14:40:07 -0300

    dlAlien_package_JBs.sh - corrected some shell script errors

--- dlAlien_package_JBs.sh
+++ dlAlien_package_JBs.sh
@@ -22,24 +22,24 @@
 #
 # Script: Download files/packages from one mirror with MD5
 #
-# Last update: 03/02/2017
+# Last update: 14/04/2017
 #
-case "$( uname -m )" in
+case "$(uname -m)" in
     i?86) archDL=x86 ;;
-    *) archDL=$( uname -m ) ;;
+    *) archDL=$(uname -m) ;;
 esac
 
 echo -e "\n# This script download files/packages from one alien mirror #\n"
 echo -e "### Use \"pathDl\"- to download the packages instead the full folder ###"
 
-pathDl=$1
+pathDl="$1"
 if [ "$pathDl" == '' ]; then
     echo -n "T2ype the path/program that want download: "
-    read pathDl
+    read -r pathDl
 fi
 
 echo -en "\nOnly \"t?z\" or all files? 1 to only \"t?z\" - 2 to all (hit enter to only t?z): "
-read allOrNot
+read -r allOrNot
 
 if [ "$allOrNot" != '2' ]; then
     extensionFile=".t\?z"
@@ -52,22 +52,22 @@ mirrorDl="$mirrorStart/$slackVersion/$archDL"
 
 wget "$mirrorDl/CHECKSUMS.md5" -O CHECKSUMS.md5
 
-runFile=`cat CHECKSUMS.md5 | grep "$pathDl.*.$extensionFile$" | cut -d '.' -f2-`
+runFile=$(grep "$pathDl.*.$extensionFile$" < CHECKSUMS.md5 | cut -d '.' -f2-)
 rm CHECKSUMS.md5
 
 echo -e "Will download the file(s) listed: \n$runFile\n"
 echo -n "Want to continue? (y)es - (n)o (hit enter to yes): "
-read continueDl
+read -r continueDl
 
 if [ "$continueDl" != "n" ]; then
-    mkdir $pathDl-new
-    cd $pathDl-new
+    mkdir "${pathDl}-new"
+    cd "${pathDl}-new" || exit
 
-    for fileGrep in `echo -e "$runFile"`; do
-        wget -c $mirrorDl/$fileGrep
+    for fileGrep in $(echo -e "$runFile"); do
+        wget -c "$mirrorDl/$fileGrep"
     done
 
-    echo -e "List of files downloaded:\n\n`tree --noreport`\n"
+    echo -e "List of files downloaded:\n\n$(tree --noreport)\n"
 else
     echo -e "\nJust exiting by user choice\n"
 fi
