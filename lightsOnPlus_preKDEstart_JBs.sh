e11b2bd5587de70bdfdeab502ef31005f19b0689
João Batista <joao42lbatista@gmail.com>
2017-04-14 22:29:24 -0300

    lightsOnPlus_preKDEstart_JBs.sh - corrected some shell script errors

--- lightsOnPlus_preKDEstart_JBs.sh
+++ lightsOnPlus_preKDEstart_JBs.sh
@@ -1,7 +1,7 @@
-# !/bin/bash
+#!/bin/bash
 #
 # Use this scrpit to load lightsOnPlusJB.sh in the KDE pre-start
-# Last update: 08/06/2016
+# Last update: 14/04/2017
 #
 # Don't forget to copy lightsOnPlusJB.sh to /usr/bin/
 # You can use the ./0install.r to do this
