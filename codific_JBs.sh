bb5252fb7277297f4e3c64bd33cc1723dade7610
João Batista <joao42lbatista@gmail.com>
2017-04-14 22:24:03 -0300

    codific_JBs.sh - corrected some shell script errors

--- codific_JBs.sh
+++ codific_JBs.sh
@@ -20,11 +20,11 @@
 #
 # Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 #
-# Script: converte texto utf8 to iso-8859 e virse-versa
+# Script: converte texto utf8 to iso-8859 e vice-versa
 #
-# Last update: 07/04/2016
+# Last update: 14/04/2017
 #
-if [ $# -ne 1 ]; then # Verifica se foi passado o nome do arquivo
+if [ "$#" -ne '1' ]; then # Verifica se foi passado o nome do arquivo
     echo -e "\n$(basename "$0"): Erro de operandos"
     echo "Use $0 nome.extensão (do arquivo que deseja converter)"
     echo -e "Tente $0 --help para mais detalhes\n"
@@ -37,48 +37,48 @@ ajuda () { # Função de Ajuda
     exit 0
 }
 
-case "$1" in # Case virifica chamada da função ajuda
-'--help')
-    ajuda
+case $1 in # Case verifica chamada da função ajuda
+    '--help')
+        ajuda
 esac
 
-nomeArquivo="$1" # Nome do arquivo $1
+nomeArquivo=$1 # Nome do arquivo $1
 if [ ! -e "$nomeArquivo" ]; then
     echo -e "\nArquivo passado por parâmetro não existe\nTente \"$0 --help\" ou com outro arquivo\n"
     exit 1
 fi
 
-tamanhoNome=$(echo "$nomeArquivo" | wc -m) # Calcula o tamanho do Nome e retirando retorno do wc
+tamanhoNome=${#nomeArquivo} # Calcula o tamanho do nome
 tamanhoNome2=$((tamanhoNome - 5)) # Tamanho do nome do arquivo sem a extensão
 nomeArquivo2=$(echo "$nomeArquivo" | cut -c1-$tamanhoNome2) # Nome do arquivo sem extensão
-tamanhoNome2=$((tamanhoNome - 3)) # Tamanho do nome do arquivo sem a extensão pra pegar a extensão
+tamanhoNome2=$((tamanhoNome - 2)) # Tamanho do nome do arquivo sem a extensão pra pegar a extensão
 extensao=$(echo "$nomeArquivo" | cut -c$tamanhoNome2-) # Extensão do arquivo
-codificacao=$(cat "$nomeArquivo" | file -) # Verifica codificação do arquivo
+codificacao=$(file "$nomeArquivo") # Verifica codificação do arquivo
 
-if echo $codificacao | grep -q ISO-8859; then # Verifica codificação do arquivo é iso-8859
-    codInicial=iso-8859
-    codFinal=utf-8
-    iconv -f iso-8859-1 -t utf-8 "$nomeArquivo" > "$nomeArquivo2"_"$codFinal"."$extensao" # Converte arquivo para utf-8 salvando em outro arquivo
-elif echo $codificacao | grep -q UTF-8; then # Verifica codificacao do arquivo é iso-8859
-    codInicial=utf-8
-    codFinal=iso-8859
-    iconv -f utf-8 -t iso-8859-1 "$nomeArquivo" > "$nomeArquivo2"_"$codFinal"."$extensao" # Converte arquivo para iso-8859 salvando em outro arquivo
-else # Em último caso, se o arquivo não for de nenhuma das duas codificações, o sccrit termina
+if echo "$codificacao" | grep -q "ISO-8859"; then # Verifica codificação do arquivo é iso-8859
+    codInicial="iso-8859"
+    codFinal="utf-8"
+    iconv -f iso-8859-1 -t utf-8//TRANSLIT "$nomeArquivo" > "$nomeArquivo2"_"$codFinal"."$extensao" # Converte arquivo para utf-8 salvando em outro arquivo
+elif echo "$codificacao" | grep -q "UTF-8"; then # Verifica codificação do arquivo é iso-8859
+    codInicial="utf-8"
+    codFinal="iso-8859"
+    iconv -f utf-8 -t iso-8859-1//TRANSLIT "$nomeArquivo" > "$nomeArquivo2"_"$codFinal"."$extensao" # Converte arquivo para iso-8859 salvando em outro arquivo
+else # Em último caso, se o arquivo não for de nenhuma das duas codificações, o script termina
     echo -e "\nCodificação desconhecida\nArquivo não pode ser convertido\n"
     exit 1
 fi
 
-if [ $? -eq 1 ]; then
+if [ "$?" -eq '1' ]; then
     echo -e "Erro encontrado na execução do iconv\nTente $0 --help"
     exit 1
 else
     echo -e "\n## Arquivo convertido com sucesso ##\n\"$nomeArquivo\" de $codInicial para $codFinal"
-    echo "$nomeArquivo --> $nomeArquivo2"_"$codFinal.$extensao"
+    echo "$nomeArquivo --> ${nomeArquivo2}_${codFinal}.$extensao"
 
     echo -en "\nSobrescrever o arquivo original?\n(y)es, (n)o: "
-    read resposta
-    if [ "$resposta" = 'y' ]; then
-        mv "$nomeArquivo2"_"$codFinal.$extensao" "$nomeArquivo"
+    read -r resposta
+    if [ "$resposta" == 'y' ]; then
+        mv "${nomeArquivo2}_${codFinal}.$extensao" "$nomeArquivo"
         echo -e "O arquivo foi sobrescrito\n Fim do script\n"
     else
         echo -e "O arquivo não foi sobrescrito\n Fim do script\n"
