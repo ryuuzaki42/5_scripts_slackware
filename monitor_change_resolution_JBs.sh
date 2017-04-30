26611065c334aaf53356b9213566e91f0c6df9b9
João Batista <joao42lbatista@gmail.com>
2017-04-30 14:41:09 -0300

    monitor_change_resolution_JBs.sh - corrected some shell script errors

--- monitor_change_resolution_JBs.sh
+++ monitor_change_resolution_JBs.sh
@@ -20,46 +20,46 @@
 #
 # Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 #
-# Script: altera resolução do seu monitor e projetor
+# Script: Change the resolution of the monitor or/and projector
 #
-# Last update: 10/10/2016
+# Last update: 30/04/2017
 #
 echo -e "\nScript to change the resolution of your display (LVDS1), output (VGA1) and output HDMI (HDMI1)\n"
 
 # Initialize variables
-LVDS1_status=false
-VGA1_status=false
-HDMI1_status=false # Implementation in te future
+LVDS1_status="false"
+VGA1_status="false"
+HDMI1_status="false" # Implementation in the future
 
 echo -e "\t--------------------------"
 echo -e "\t# Input # Status       #"
 
 if xrandr | grep -q "LVDS1 connected"; then
     echo -e "\t# LVDS1   # connected    #"
-    LVDS1_status=true
-    LVDS1_resolution=`xrandr | grep \+ | grep -v "+0" | grep -v "connected" | cut -d' ' -f4 | sed -n "1p"`
+    LVDS1_status="true"
+    LVDS1_resolution=$(xrandr | grep \+ | grep -v "+0" | grep -v "connected" | cut -d ' ' -f4 | sed -n "1p")
 else
     echo -e "\t# LVDS1   # disconnected #"
 fi
 
 if xrandr | grep -q "VGA1 connected"; then
     echo -e "\t# VGA1    # connected    #"
-    VGA1_status=true
-    VGA1_resolution=`xrandr | grep \+ | grep -v "+0" | grep -v "connected" | cut -d' ' -f4 | sed -n "2p"`
+    VGA1_status="true"
+    VGA1_resolution=$(xrandr | grep \+ | grep -v "+0" | grep -v "connected" | cut -d ' ' -f4 | sed -n "2p")
 else
     echo -e "\t# VGA1    # disconnected #"
 fi
 
 if xrandr | grep -q "HDMI1 connected"; then
     echo -e "\t# HDMI1   # connected    #"
-    HDMI1_status=true
-    HDMI1_resolution=`xrandr  | grep \+ | grep -v "+0" | grep -v "connected" | cut -d' ' -f4 | sed -n "3p"`
+    HDMI1_status="true"
+    HDMI1_resolution=$(xrandr  | grep \+ | grep -v "+0" | grep -v "connected" | cut -d ' ' -f4 | sed -n "3p")
 else
     echo -e "\t# HDMI1   # disconnected #"
 fi
 echo -e "\t--------------------------\n"
 
-if [ $VGA1_status == true ]; then
+if [ "$VGA1_status" == "true" ]; then
     echo "1 - LVDS1 $LVDS1_resolution on, VGA1 off"
     echo "2 - LVDS1 off, VGA1 $VGA1_resolution on"
     echo "3 - LVDS1 e VGA1 1024x768 (mirror)"
@@ -69,7 +69,7 @@ if [ $VGA1_status == true ]; then
     echo "f - Finish or terminate"
 
     echo -e "\nWith option you wish?"
-    read optionSelected
+    read -r optionSelected
     if [ "$optionSelected" == '' ]; then
         echo -e "\n\tError: You need select one of the option listed\n"
         optionSelected="" # Setting an option that is not valid to jump the case in front
@@ -79,17 +79,17 @@ else
     optionSelected="" # Setting an option that is not valid to jump the case in front
 fi
 
-if [ "$optionSelected" == 4 ] || [ "$optionSelected" == 5 ]; then # Only for the option 4 and 5
+if [ "$optionSelected" == '4' ] || [ "$optionSelected" == '5' ]; then # Only for the option 4 and 5
     # LVDS1 part resolution
-    LVDS1_resolution_Part1=`echo $LVDS1_resolution | cut -d 'x' -f1`
-    LVDS1_resolution_Part2=`echo $LVDS1_resolution | cut -d 'x' -f2`
+    LVDS1_resolution_Part1=$(echo "$LVDS1_resolution" | cut -d 'x' -f1)
+    LVDS1_resolution_Part2=$(echo "$LVDS1_resolution" | cut -d 'x' -f2)
 
     # VGA1 part resolution
-    VGA1_resolution_Part1=`echo $VGA1_resolution | cut -d 'x' -f1`
-    VGA1_resolution_Part2=`echo $VGA1_resolution | cut -d 'x' -f2`
+    VGA1_resolution_Part1=$(echo "$VGA1_resolution" | cut -d 'x' -f1)
+    VGA1_resolution_Part2=$(echo "$VGA1_resolution" | cut -d 'x' -f2)
 
     # Diff VGA1_p2 - LVDS1_p2
-    diffResolutionPart2=`echo "$VGA1_resolution_Part2 - $LVDS1_resolution_Part2" | bc`
+    diffResolutionPart2=$(echo "$VGA1_resolution_Part2 - $LVDS1_resolution_Part2" | bc)
 
     if [ "$1" != '' ]; then ## Test propose
         echo -e "\nLVDS1: $LVDS1_resolution_Part1 x $LVDS1_resolution_Part2"
@@ -100,11 +100,11 @@ fi
 
 case $optionSelected in
     1 )
-        xrandr --output LVDS1 --mode $LVDS1_resolution --primary
+        xrandr --output LVDS1 --mode "$LVDS1_resolution" --primary
         xrandr --output VGA1 --off
         ;;
     2 )
-        xrandr --output VGA1 --mode $VGA1_resolution --primary
+        xrandr --output VGA1 --mode "$VGA1_resolution" --primary
         xrandr --output LVDS1 --off
         ;;
     3 )
@@ -112,10 +112,10 @@ case $optionSelected in
         xrandr --output VGA1 --mode 1024x768 --same-as LVDS1
         ;;
     4 )
-        xrandr --output LVDS1 --mode $LVDS1_resolution --primary --pos 0x$diffResolutionPart2 --output VGA1 --mode $VGA1_resolution --pos "$LVDS1_resolution_Part1"x0
+        xrandr --output LVDS1 --mode "$LVDS1_resolution" --primary --pos "0x$diffResolutionPart2" --output VGA1 --mode "$VGA1_resolution" --pos "${LVDS1_resolution_Part1}x0"
         ;;
     5 )
-        xrandr --output LVDS1 --mode $LVDS1_resolution --pos "$VGA1_resolution_Part1"x"$diffResolutionPart2" --output VGA1 --mode $VGA1_resolution --primary --pos 0x0
+        xrandr --output LVDS1 --mode "$LVDS1_resolution" --pos "${VGA1_resolution_Part1}x$diffResolutionPart2" --output VGA1 --mode "$VGA1_resolution" --primary --pos 0x0
         ;;
     0 )
         echo -e "\n6 - LVDS1 left-of VGA1"
@@ -127,14 +127,14 @@ case $optionSelected in
         echo "f - Finish or terminate"
 
         echo -e "\nWith option you wish?"
-        read optionSelected
+        read -r optionSelected
         if [ "$optionSelected" == '' ]; then
             echo -e "\n\tError: You need select one of the option listed\n"
             optionSelected="" # Seting a option that is not valid to jump the case in front
         fi
 
-        LVDS1_resolution_actual=`xrandr | grep "LVDS1" | sed 's/ primary//' | cut -d" " -f3 | cut -d"+" -f1`
-        VGA1_resolution_actual=`xrandr | grep "VGA1" | sed 's/ primary//' | cut -d" " -f3 | cut -d"+" -f1`
+        LVDS1_resolution_actual=$(xrandr | grep "LVDS1" | sed 's/ primary//' | cut -d " " -f3 | cut -d "+" -f1)
+        VGA1_resolution_actual=$(xrandr | grep "VGA1" | sed 's/ primary//' | cut -d " " -f3 | cut -d "+" -f1)
 
         if [ "$1" != '' ]; then ## Test propose
             echo -e "\nLVDS1: $LVDS1_resolution_actual"
@@ -142,34 +142,34 @@ case $optionSelected in
         fi
 
         # LVDS1 part resolution
-        LVDS1_resolution_Part1=`echo $LVDS1_resolution_actual | cut -d 'x' -f1`
-        LVDS1_resolution_Part2=`echo $LVDS1_resolution_actual | cut -d 'x' -f2`
+        LVDS1_resolution_Part1=$(echo "$LVDS1_resolution_actual" | cut -d 'x' -f1)
+        LVDS1_resolution_Part2=$(echo "$LVDS1_resolution_actual" | cut -d 'x' -f2)
 
         # VGA1 part resolution
-        VGA1_resolution_Part1=`echo $VGA1_resolution_actual | cut -d 'x' -f1`
-        VGA1_resolution_Part2=`echo $VGA1_resolution_actual | cut -d 'x' -f2`
+        VGA1_resolution_Part1=$(echo "$VGA1_resolution_actual" | cut -d 'x' -f1)
+        VGA1_resolution_Part2=$(echo "$VGA1_resolution_actual" | cut -d 'x' -f2)
 
         # Diff VGA1_p2 - LVDS1_p2
-        if echo $LVDS1_resolution_actual | grep -q [[:digit:]]; then # Test if LVDS1 is ative
-            if echo $VGA1_resolution_actual | grep -q [[:digit:]]; then # Test if VGA1 is ative
-                diffResolutionPart2=`echo "$VGA1_resolution_Part2 - $LVDS1_resolution_Part2" | bc`
+        if echo "$LVDS1_resolution_actual" | grep -q "[[:digit:]]"; then # Test if LVDS1 is ative
+            if echo "$VGA1_resolution_actual" | grep -q "[[:digit:]]"; then # Test if VGA1 is ative
+                diffResolutionPart2=$(echo "$VGA1_resolution_Part2 - $LVDS1_resolution_Part2" | bc)
             else
                 echo -e "\n\tError: VGA1 is not active\n"
-                LVDS1orVGA1NotAtive=1
+                LVDS1orVGA1NotAtive='1'
             fi
         else
             echo -e "\n\tError: LVDS1 is not active\n"
-            LVDS1orVGA1NotAtive=1
+            LVDS1orVGA1NotAtive='1'
         fi
 
-        if [ "$LVDS1orVGA1NotAtive" == 1 ]; then
+        if [ "$LVDS1orVGA1NotAtive" == '1' ]; then
             echo "What you want, set the maximum resolution for both and continue or just terminate?"
             echo -en "\t(s)et the maximum resolution - (t)erminate: "
-            read continueOrNot
+            read -r continueOrNot
 
             if [ "$continueOrNot" == 's' ]; then # Set the maximum resolution for both and continue
-                xrandr --output LVDS1 --mode $LVDS1_resolution
-                xrandr --output VGA1 --mode $VGA1_resolution
+                xrandr --output LVDS1 --mode "$LVDS1_resolution"
+                xrandr --output VGA1 --mode "$VGA1_resolution"
             else # Just terminate
                 optionSelected="" # Setting an option that is not valid to jump the case in front
             fi
@@ -183,10 +183,10 @@ case $optionSelected in
 
         case $optionSelected in
             6 )
-                xrandr --output LVDS1 --pos 0x$diffResolutionPart2 --output VGA1 --pos "$LVDS1_resolution_Part1"x0
+                xrandr --output LVDS1 --pos "0x$diffResolutionPart2" --output VGA1 --pos "${LVDS1_resolution_Part1}x0"
                 ;;
             7 )
-                xrandr --output LVDS1 --pos "$VGA1_resolution_Part1"x"$diffResolutionPart2" --output VGA1 --pos 0x0
+                xrandr --output LVDS1 --pos "${VGA1_resolution_Part1}x$diffResolutionPart2" --output VGA1 --pos 0x0
                 ;;
             8 )
                 xrandr --output LVDS1 --above VGA1
