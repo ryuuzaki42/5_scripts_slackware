2cc500b248d80ad380f97f14449b495cb01ceca7
João Batista <joao42lbatista@gmail.com>
2017-07-14 13:55:32 -0300

    lightsOnPlus_preKDEstart_JBs.sh - Deleted

--- lightsOnPlus_preKDEstart_JBs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Use this scrpit to load lightsOnPlusJB.sh in the KDE pre-start
-# Last update: 14/04/2017
-#
-# Don't forget to copy lightsOnPlusJB.sh to /usr/bin/
-# You can use the ./0install.r to do this
-#
-/usr/bin/lightsOnPlus_JBs.sh &
