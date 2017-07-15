fe1c37080583122189046fddaef1e42a83910f5a
João Batista <joao42lbatista@gmail.com>
2017-07-15 12:09:31 -0300

    slackwareForCheckUpdates_JBs.sh - Remove "f - to use \"file://dir/\" or \"cdrom://dir/\" as the mirror", now test (if exist the folder) and use by default/automatic - Improved/small changes

--- slackwareForCheckUpdates_JBs.sh
+++ slackwareForCheckUpdates_JBs.sh
@@ -27,14 +27,13 @@
 # Last update: 15/07/2017
 #
 echo -e "\n# Script to check for Slackware updates #"
-echo -e "\t$(basename "$0") h - for help"
 
 optionInput=$1
 if [ "$optionInput" == 'h' ]; then
     echo -e "\n# If has a mirror not valid in \"/etc/slackpkg/mirrors\" you can use:"
+    echo -e "\t$(basename "$0") h - this help message"
     echo -e "\t$(basename "$0") s - to select one mirror from stable version"
     echo -e "\t$(basename "$0") c - to select one mirror from current"
-    echo -e "\t$(basename "$0") f - to use \"file://dir/\" or \"cdrom://dir/\" as the mirror"
     echo -e "\t$(basename "$0") n - to insert your favorite mirror"
 fi
 
@@ -66,81 +65,90 @@ tracePrint () {
 
 getUpdateMirror () {
     mirrorDl=$1
-    echo -e "Download the \"ChangeLog.txt\" from: \"$mirrorDl\". Please wait...\n"
+    echo -e "\nGetting the \"ChangeLog.txt\" from: \"$mirrorDl\". Please wait..."
 
-    if [ "$optionInput" == 'f' ]; then
+    mirrorDlTest=$(echo "$mirrorDl" | cut -d "/" -f1)
+    if [ "$mirrorDlTest" == "file:" ] || [ "$mirrorDlTest" == "cdrom:" ]; then
+        mirrorDl=$(echo "$mirrorDl" | cut -d '/' -f2-)
+
         cp "$mirrorDl/ChangeLog.txt" "$(pwd)"
     else
         wget "${mirrorDl}ChangeLog.txt"
     fi
     changePkgs=$(grep -E "txz|tgz|\+---|UTC" ChangeLog.txt)
 
-    count1="25"
-    count2="55"
+    if [ "$countPkg" != '' ]; then
+        count1="25"
+        count2="55"
 
-    echo
-    tracePrint "$count1" "$count2"
-    alinPrint "Package Name" "$count1"
-    alinPrint "Version installed" "$count2"
-    alinPrint "Update available" "$count2"
-    alinPrint "Summary" "$count1"
-    echo "#"
-    tracePrint "$count1" "$count2"
+        echo
+        tracePrint "$count1" "$count2"
+        alinPrint "Package Name" "$count1"
+        alinPrint "Version installed" "$count2"
+        alinPrint "Update available" "$count2"
+        alinPrint "Summary" "$count1"
+        echo "#"
+        tracePrint "$count1" "$count2"
 
-    for value in $changePkgs; do
-        if echo "$value" | grep -qE "txz|tgz"; then # Find one package to update
-            packageName=$(echo "$value" | cut -d ':' -f1 | rev | cut -d '/' -f1 | cut -d '-' -f4- | rev)
-            packageNameUpdate=$(echo "$value" | cut -d ':' -f1 | rev | cut -d '/' -f1 | rev)
+        for value in $changePkgs; do
+            if echo "$value" | grep -qE "txz|tgz"; then # Find one package to update
+                packageName=$(echo "$value" | cut -d ':' -f1 | rev | cut -d '/' -f1 | cut -d '-' -f4- | rev)
+                packageNameUpdate=$(echo "$value" | cut -d ':' -f1 | rev | cut -d '/' -f1 | rev)
 
-            packageVersionInstalled=$(find /var/log/packages/ | grep "/$packageName-" | rev | cut -d '/' -f1 | rev)
-            countPkg=$(echo -e "$packageVersionInstalled" | wc -l) # To test if found more than one package with $packageName
-            countPkgTmp='1'
-            for pkg in $packageVersionInstalled; do
-                pkgTmp=$(echo "$pkg" | rev | cut -d '-' -f4- | rev)
+                packageVersionInstalled=$(find /var/log/packages/ | grep "/$packageName-" | rev | cut -d '/' -f1 | rev)
+                countPkg=$(echo -e "$packageVersionInstalled" | wc -l) # To test if found more than one package with $packageName
+                countPkgTmp='1'
+                for pkg in $packageVersionInstalled; do
+                    pkgTmp=$(echo "$pkg" | rev | cut -d '-' -f4- | rev)
 
-                if [ "$pkgTmp" == "$packageName" ]; then
-                    packageVersionInstalled=$pkg
-                else
-                    if [ "$countPkg" == "$countPkgTmp" ]; then
-                        packageVersionInstalled=''
-                    fi
-                fi
-
-                ((countPkgTmp++))
-            done
-
-            versionUpdate=$(echo "$packageNameUpdate" | rev | cut -d '-' -f3 | rev)
-            versionInstalled=$(echo "$packageVersionInstalled" | rev | cut -d '-' -f3 | rev)
-
-            packageNameUpdateTmp=$(echo "$packageNameUpdate" | rev | cut -d '.' -f2- | rev)
-            locatePackage=$(find /var/log/packages/ | grep "$packageNameUpdateTmp")
-
-            if [ "$locatePackage" == '' ]; then # To no print the last package (the already update in the Slackware)
-                if [ "$packageVersionInstalled" != '' ]; then # To print only if the package has another version installed
-                    alinPrint "$packageName" "$count1"
-                    alinPrint "$packageVersionInstalled" "$count2"
-                    alinPrint "$packageNameUpdate" "$count2"
-
-                    if [ "$versionInstalled" == "$versionUpdate" ]; then
-                        alinPrint "Rebuilt" "$count1"
+                    if [ "$pkgTmp" == "$packageName" ]; then
+                        packageVersionInstalled=$pkg
                     else
-                        alinPrint "$versionInstalled to $versionUpdate" "$count1"
+                        if [ "$countPkg" == "$countPkgTmp" ]; then
+                            packageVersionInstalled=''
+                        fi
                     fi
 
-                    echo "#"
-                    tracePrint "$count1" "$count2"
+                    ((countPkgTmp++))
+                done
+
+                versionUpdate=$(echo "$packageNameUpdate" | rev | cut -d '-' -f3 | rev)
+                versionInstalled=$(echo "$packageVersionInstalled" | rev | cut -d '-' -f3 | rev)
+
+                packageNameUpdateTmp=$(echo "$packageNameUpdate" | rev | cut -d '.' -f2- | rev)
+                locatePackage=$(find /var/log/packages/ | grep "$packageNameUpdateTmp")
+
+                if [ "$locatePackage" == '' ]; then # To no print the last package (the already update in the Slackware)
+                    if [ "$packageVersionInstalled" != '' ]; then # To print only if the package has another version installed
+                        alinPrint "$packageName" "$count1"
+                        alinPrint "$packageVersionInstalled" "$count2"
+                        alinPrint "$packageNameUpdate" "$count2"
+
+                        if [ "$versionInstalled" == "$versionUpdate" ]; then
+                            alinPrint "Rebuilt" "$count1"
+                        else
+                            alinPrint "$versionInstalled to $versionUpdate" "$count1"
+                        fi
+
+                        echo "#"
+                        tracePrint "$count1" "$count2"
+                    fi
+                else
+                    valueToStopPrint=$packageNameUpdateTmp
+                    break
                 fi
-            else
-                valueToStopPrint=$packageNameUpdateTmp
-                break
             fi
-        fi
-    done
+        done
 
-    changesToShow=$(sed '/'"$valueToStopPrint"'/q' ChangeLog.txt)
-    rm ChangeLog.txt
+        changesToShow=$(sed '/'"$valueToStopPrint"'/q' ChangeLog.txt)
+        rm ChangeLog.txt
 
-    countLines=$(echo "$changesToShow" | grep -n "\+---" | tail -n 1 | cut -d: -f1)
+        countLines=$(echo "$changesToShow" | grep -n "\+---" | tail -n 1 | cut -d: -f1)
+    else
+        rm ChangeLog.txt
+        countLines=''
+    fi
+
     if [ "$countLines" != '' ]; then
         updaesAvailable=$(echo "$changesToShow" | head -n "$countLines")
         updaesAvailable=$(echo -e "\n+--------------------------+\n$updaesAvailable")
@@ -149,12 +157,12 @@ getUpdateMirror () {
         updaesAvailable=${updaesAvailable//'"'/'\"'} # Change " to \" go get error with "echo "notify-send""
 
         iconName="audio-volume-high"
-        notificationToSend=$(echo -e "notify-send \"$(basename "$0")\n Updates available\" \"$updaesAvailable\" -i \"$iconName\"")
+        notificationToSend=$(echo -e "notify-send \"$(basename "$0")\n\n Updates available\" \"$updaesAvailable\" -i \"$iconName\"")
     else
         echo -e "\n\t# Updates not found #"
 
         iconName="audio-volume-muted"
-        notificationToSend=$(echo -e "notify-send \"$(basename "$0")\n Updates not found\" \"No news is good news\" -i \"$iconName\"")
+        notificationToSend=$(echo -e "notify-send \"$(basename "$0")\n\n Updates not found\" \"No news is good news\" -i \"$iconName\"")
     fi
 
     eval "$notificationToSend"
@@ -164,67 +172,77 @@ getUpdateMirror () {
 getValidMirror () {
     mirrorDl=$(grep -v "#" /etc/slackpkg/mirrors)
 
-    if [ "$optionInput" == 'f' ]; then
-        mirrorDlTest=$(echo "$mirrorDl" | cut -d ":" -f 1)
+    if [ "$mirrorDl" != '' ]; then
+        echo -e "\nMirror active in \"/etc/slackpkg/mirrors\":\n\"$mirrorDl\""
+    else
+        echo -e "\nThere is no mirror active in \"/etc/slackpkg/mirrors\""
+        echo -e "\t# Please active one mirror #"
+    fi
 
-        if [ "$mirrorDlTest" == "file:" ] || [ "$mirrorDlTest" == "cdrom:" ]; then
-            mirrorDl=$(echo "$mirrorDl" | cut -d '/' -f2-)
-        else
-            optionInput=''
+    mirrorDlTest=$(echo "$mirrorDl" | cut -d "/" -f1)
+    if [ "$mirrorDlTest" == "file:" ] || [ "$mirrorDlTest" == "cdrom:" ]; then
+        mirrorDl=$(echo "$mirrorDl" | cut -d '/' -f2-)
+
+        if [ ! -x "$mirrorDl" ]; then
+            echo -e "\nThe mirror folder don't exist: \"$mirrorDl\""
+            mirrorTest='1'
+        fi
+    else
+        mirrorTest='1'
+    fi
+
+    if [ "$mirrorTest" == '1' ]; then
+        if echo "${mirrorDl}" | grep -vqE "^http:|^ftp:"; then
+            if [ "$mirrorDl" != '' ]; then
+                echo -e "\t# This mirror is not valid #"
+            fi
+
+            slackwareVersion=$(grep "VERSION=" /etc/os-release | cut -d '"' -f2)
+            slackwareArch=$(uname -m)
+
+            mirrorPart1="ftp://mirrors.slackware.com/slackware/"
+            mirrorCurrent="${mirrorPart1}slackware64-current/"
+
+            if echo "$slackwareArch" | grep -q "64"; then
+                mirrorPart2="slackware64-$slackwareVersion/"
+            else
+                mirrorPart2="slackware-$slackwareVersion/"
+            fi
+            mirrorFinal=$mirrorPart1$mirrorPart2
+
+            if [ "$optionInput" == '' ]; then
+                echo -e "\nSuggested mirror to use:"
+                echo "s - (stable) - $mirrorFinal"
+                echo "c - (current) - $mirrorCurrent"
+                echo "n - Or insert your favorite mirror"
+                echo -n "Which mirror you want?: "
+                read -r optionInput
+            fi
+
+            if [ "$optionInput" == 's' ]; then
+                mirrorDl=$mirrorFinal
+            elif [ "$optionInput" == 'c' ]; then
+                mirrorDl=$mirrorCurrent
+            else
+                mirrorSource=''
+                testMirrorCommand="echo \"\$mirrorSource\" | grep -vqE \"^ftp:|^http:|^file:|^cdrom:\""
+                while eval $testMirrorCommand; do
+                    echo -en "\nType the new mirror: "
+                    read -r mirrorSource
+
+                    if eval $testMirrorCommand; then
+                        echo -e "\nError: the mirror \"$mirrorSource\" is not valid"
+                        echo "One valid mirror has \"ftp:\", \"http:\", \"file:\" or \"cdrom:\""
+                    fi
+                done
+
+                echo -e "\nNew mirror: $mirrorSource"
+                mirrorDl=$mirrorSource
+            fi
         fi
     fi
 
-    if echo "${mirrorDl}" | grep -vqE "^http:|^ftp:"; then
-        if [ "$mirrorDl" != '' ]; then
-            echo -e "\nMirror active in \"/etc/slackpkg/mirrors\": \"$mirrorDl\""
-            echo -e "\t# This mirror is not valid #"
-        else
-            echo -e "\nThere is no Mirror active in \"/etc/slackpkg/mirrors\""
-            echo -e "\t# Please active one mirror #"
-        fi
-
-        slackwareVersion=$(grep "VERSION=" /etc/os-release | cut -d '"' -f2)
-        slackwareArch=$(uname -m)
-
-        mirrorPart1="ftp://mirrors.slackware.com/slackware/"
-        mirrorCurrent="${mirrorPart1}slackware64-current/"
-
-        if echo "$slackwareArch" | grep -q "64"; then
-            mirrorPart2="slackware64-$slackwareVersion/"
-        else
-            mirrorPart2="slackware-$slackwareVersion/"
-        fi
-        mirrorFinal=$mirrorPart1$mirrorPart2
-
-        if [ "$optionInput" == '' ]; then
-            echo -e "\nSuggested mirror to use:"
-            echo "s - (stable) - $mirrorFinal"
-            echo "c - (current) - $mirrorCurrent"
-            echo "n - Or insert your favorite mirror"
-            echo -n "Which mirror you want?: "
-            read -r optionInput
-        fi
-
-        if [ "$optionInput" == 's' ]; then
-            mirrorDl=$mirrorFinal
-        elif [ "$optionInput" == 'c' ]; then
-            mirrorDl=$mirrorCurrent
-        else
-            mirrorSource=''
-            while echo "$mirrorSource" | grep -vqE "ftp|http"; do
-                echo -en "$CYAN \nType the new mirror:$NC "
-                read -r mirrorSource
-
-                if echo "$mirrorSource" | grep -vqE "ftp|http"; then
-                    echo -e "$RED\nError: the mirror \"$mirrorSource\" is not valid.\nOne valid mirror has \"ftp\" or \"http\"$NC"
-                fi
-            done
-
-            echo -e "$CYAN\nNew mirror:$GREEN $mirrorSource$NC"
-            mirrorDl=$mirrorSource
-        fi
-    fi
-    echo -e "\nUsing the mirror: $mirrorDl\n"
+    echo -e "\nUsing the mirror: \"$mirrorDl\""
 }
 
 getValidMirror "$optionInput"
