57c64c06bf9eb0755e60cf5f26a49e14ec508009
João Batista <joao42lbatista@gmail.com>
2018-01-15 17:01:32 -0200

    volume_pulse_JBs.sh - Added the support to find try find the sound device in use and corrected the bug with down volume percentage

--- volume_pulse_JBs.sh
+++ volume_pulse_JBs.sh
@@ -22,25 +22,32 @@
 #
 # Script: Change the volume percentage and send notification (if wanted)
 #
-# Last update: 14/11/2017
+# Last update: 15/01/2018
 #
 help() {
-    echo -e "\\nUsage: $0 \"soundDevice\" [up|down|min|max|overmax]"
+    echo -e "\\nHelp message:\\nUsage: $0 \"soundDevice\" [up|down|min|max|overmax]"
     echo -e "You can add '0' (zero) at the end the command to not send notification\\n"
+    echo -e "If not pass the \"soundDevice\", the script will try find to device in use\\n"
     exit 0
 }
 
-if [ "$#" -lt '2' ]; then
+if [ "$#" -lt '1' ]; then
     help
 fi
 
-soundDevice=$1 # Device number - Check with: aplay -l
+soundDevice=$1 # Device number - Check with: aplay -l or pacmd list-sinks
 optionValue=$2 # Option wanted - [up|down|min|max|overmax]
 notification=$3 # Send notification? - If 0 will no send
 
 volStepChange='5'
 maxVol="100"
 volCurrentPerc=$(pacmd list-sinks | grep "volume" | head -n 1 | cut -d '/' -f2 | cut -d '%' -f1 | tr -d "[:space:]")
+
+if echo "$soundDevice" | grep -vq "[[:digit:]]"; then # Shift the value if soundDevice is not inserted
+    soundDevice=$(pacmd list-sinks | grep "index" | cut -d ':' -f2 | tr -d "[:space:]")
+    optionValue=$1
+    notification=$2
+fi
 
 case $optionValue in
     "up" )
@@ -57,13 +64,20 @@ case $optionValue in
         ;;
     "down" )
         skipOverCheck='1'
-        volCurrentPerc=$((volCurrentPerc - volStepChange)) ;;
+
+        if [ "$volCurrentPerc" -gt "$volStepChange" ]; then
+            volCurrentPerc=$((volCurrentPerc - volStepChange))
+        else
+            volCurrentPerc='0'
+        fi
+        ;;
     "max" )
         volCurrentPerc=$maxVol ;;
     "min" )
         volCurrentPerc='0' ;;
     "overmax" )
         skipOverCheck='1'
+
         if [ "$volCurrentPerc" -lt "100" ]; then
             volCurrentPerc=$maxVol
         else
