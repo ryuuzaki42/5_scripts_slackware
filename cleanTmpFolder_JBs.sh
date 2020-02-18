5d725cd7ae062419bc5c3c83bb068dcdf8c2f395
João D. Batista <joao42lbatista@gmail.com>
2020-02-17 22:42:51 -0300

    cleanTmpFolder_JBs.sh - add

---
+++ cleanTmpFolder_JBs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Autor= João Batista Ribeiro
+# Bugs, Agradecimentos, Críticas "construtivas"
+# Mande me um e-mail. Ficarei Grato!
+# e-mail: joao42lbatista@gmail.com
+#
+# Este programa é um software livre; você pode redistribui-lo e/ou
+# modifica-lo dentro dos termos da Licença Pública Geral GNU como
+# publicada pela Fundação do Software Livre (FSF); na versão 2 da
+# Licença, ou (na sua opinião) qualquer versão.
+#
+# Este programa é distribuído na esperança que possa ser útil,
+# mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a
+# qualquer MERCADO ou APLICAÇÃO EM PARTICULAR.
+#
+# Veja a Licença Pública Geral GNU para mais detalhes.
+# Você deve ter recebido uma cópia da Licença Pública Geral GNU
+# junto com este programa, se não, escreva para a Fundação do Software
+#
+# Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+#
+# Script: Clean /tmp/
+#
+# Last update: 17/02/2020
+#
+cd /tmp/
+
+rm -r skype-*/ 2> /dev/null
+rm -r SBo/ 2> /dev/null
+rm -r skypeforlinux*/ 2> /dev/null
+rm -r smartsynchronize-* 2> /dev/null
+rm lastChance* 2> /dev/null
+rm -r .esd-1000/ 2> /dev/null
+rm -r runtime-*/ 2> /dev/null
+rm -r dumps/ 2> /dev/null
+rm qtsingleapp-* 2> /dev/null
+rm .ktorrent_kde4_* 2> /dev/null
+rm -r Temp-*/ 2> /dev/null
+rm -r hsperfdata_*/ 2> /dev/null
+rm dropbox-antifreeze-* 2> /dev/null
+rm -r .vbox-*-ipc/ 2> /dev/null
+rm -r gameoverlayui.log 2> /dev/null
+rm -r gameoverlayui.log.last 2> /dev/null
